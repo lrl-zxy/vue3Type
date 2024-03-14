91af85f823aedc295d25c4cbd73a4b76666b3230
liruilong <920494630@qq.com>
2024-03-14 20:23:45 +0800

    leixing

--- typeScript/generics.ts
+++ typeScript/generics.ts
@@ -29,3 +29,25 @@ function echoWithLength<T extends IwithLength>(arg:T):T{
 const str = echoWithLength('string')
 const obj = echoWithLength({length:10})
 const arr = echoWithLength([1,2,3])
+
+
+class Queue<T>{
+    private data: T[] = [];
+    push(item:T){
+        return this.data.push(item)
+    }
+    pop():T{
+        return this.data.shift()
+    }
+}
+const queue = new   Queue<number>()
+
+interface KeyPair<T,U>{
+    key:T
+    value:U
+}
+
+let kp1:KeyPair<number,string> = {key:1,value:'a'}
+let kp2:KeyPair<string,number> = {key:'b',value:2}
+let arr3:number[] = [1,2,3]
+let arrTwo:Array<number> = [1,2,3]
