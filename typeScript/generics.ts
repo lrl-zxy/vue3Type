fa25fe1480e5bd8bbb22974ab67716400d8053e5
liruilong <920494630@qq.com>
2024-03-14 19:36:54 +0800

    泛型

--- typeScript/generics.ts
+++ typeScript/generics.ts
@@ -8,3 +8,24 @@ function swap<T,U>(tuple: [T,U]):[U,T]{
     return [tuple[1],tuple[0]]
 }
 const result2 = swap(['string',123])
+
+function echoWithArr<T>(arg:T[]):T[]{
+    console.log(arg.length)
+    return arg
+}
+
+const arrs = echoWithArr([1,2,3])
+
+interface IwithLength{
+    length: number
+}
+
+//约束泛型
+function echoWithLength<T extends IwithLength>(arg:T):T{
+    console.log(arg.length)
+    return arg
+}
+
+const str = echoWithLength('string')
+const obj = echoWithLength({length:10})
+const arr = echoWithLength([1,2,3])
