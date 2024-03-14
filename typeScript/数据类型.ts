dcea619014d034c0d5a38c5e640331445460cd9f
liruilong <920494630@qq.com>
2024-03-14 15:46:15 +0800

    diyijie

---
+++ typeScript/数据类型.ts
@@ -0,0 +1,11 @@
+let isDone:boolean = false;
+let a= 1
+let firstName:string = `lirui${a}`
+let u:undefined = undefined
+let n:null = null
+
+
+//尽量少使用any
+let notSure:any = 4
+notSure = 'string'
+notSure = false
