38cc6afa4b021126165e37d486dc159a579e2515
liruilong <920494630@qq.com>
2024-03-14 18:56:10 +0800

    枚举

--- typeScript/枚举 enum.js
+++ typeScript/枚举 enum.js
@@ -1,4 +1,4 @@
-//枚举类型
+//数字枚举类型
 var Direction;
 (function (Direction) {
     Direction[Direction["up"] = 0] = "up";
@@ -6,5 +6,18 @@ var Direction;
     Direction[Direction["left"] = 2] = "left";
     Direction[Direction["right"] = 3] = "right";
 })(Direction || (Direction = {}));
+//输出0
 console.log(Direction.up);
+//输出 up
 console.log(Direction[0]);
+//字符串类型枚举
+var Color;
+(function (Color) {
+    Color["red"] = "\u7EA2\u8272";
+    Color["green"] = "\u7EFF\u8272";
+    Color["blue"] = "\u84DD\u8272";
+})(Color || (Color = {}));
+var value = '红色';
+if (Color.red == value) {
+    console.log('ok');
+}
--- typeScript/枚举 enum.ts
+++ typeScript/枚举 enum.ts
@@ -1,4 +1,4 @@
-//枚举类型
+//数字枚举类型
 enum Direction{
     up,
     down,
@@ -9,3 +9,14 @@ enum Direction{
 console.log(Direction.up);
 //输出 up
 console.log(Direction[0]);
+
+//字符串类型枚举
+enum Color{
+    red='红色',
+    green='绿色',
+    blue='蓝色',
+}
+const value = '红色';
+if(Color.red==value){
+    console.log('ok');
+}
